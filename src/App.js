25cd78bbea40c6b55193f136e1f33daaa7ba712f
Nejah <nejahyengui999@gmail.com>
2021-04-02 11:48:01 +0200

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {show:true,
+      count:0,
+      person :{ fullName :"Nejah Yengui",bio:"inta3ab fi wasim wa7med", imgSrc:"logo192.png", profession:"kif kif inta3eb fi wasimm wa7med wsea3at Mohamed"
+    }
+     
+    }
+  
+  }
+  tog = ()=>this.setState({show:!this.state.show , count:0})
+  remiz=()=>this.setState({count:this.State.count=0})
+  componentDidMount(){
+    setInterval(() => {this.setState({count:this.state.count+1})
+      
+    }, 1000);
+  }
+
+  
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button id="toggle" onClick={this.tog}>{this.state.show ? "Hide":"Show"}</button>
+        {this.state.show?(<div>
+          <img src={this.state.person.imgSrc} alt="Pic"/>
+          
+        <br/>
+        {this.state.person.fullName}
+        <br/>
+        {this.state.person.bio}
+        <br/>
+        {this.state.person.profession}
+        </div>
+
+        ) : <p>cilck on show</p> }
+      </div>
+    )
+  }
 }
-
-export default App;
